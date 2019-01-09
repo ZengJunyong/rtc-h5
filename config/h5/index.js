3a30f765e2e9c35b384e3e085cbeff0c37ec1a9c
yong <zengjunyong@gmail.com>
2019-01-09 17:46:05 +0800

    build config

--- config/h5/index.js
+++ config/h5/index.js
@@ -52,7 +52,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../dist/h5'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps

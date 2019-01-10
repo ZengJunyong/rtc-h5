27c34a92836cdea47a178d1da9e4e8399b1dc66b
yong <zengjunyong@gmail.com>
2019-01-10 15:13:42 +0800

    room页面，从vue页面转化过来，还是要稍微改改

--- src/pages/room/index.vue
+++ src/pages/room/index.vue
@@ -1,43 +1,117 @@
 <template>
-  <div>
-    <ul class="container log-list">
-      <li v-for="(log, index) in logs" :class="{ red: aa }" :key="index" class="log-item">
-        <card :text="(index + 1) + ' . ' + log"></card>
-      </li>
-    </ul>
+  <div class="container flex">
+    <div class="room" :class="lengthOfRemoteVideo<=3?'':'scroll'">
+      <div class="video flex-center" :class="previewVideo?'':'opacity55'">
+        <video v-show="previewVideo" id="previewVideo" muted autoplay playsinline controls></video>
+        <img v-show="!previewVideo" src="https://zengjunyong.github.io/rtc-h5/dist/h5/static/roomset_1.png" alt="">
+      </div>
+      <div class="video flex-center" v-show="lengthOfRemoteVideo<=3? i<3: i<lengthOfRemoteVideo"
+           :class="r?'':'opacity55'"
+           v-for="(r,i) in remoteVideo">
+        <video v-show="r" class="remoteVideo" autoplay playsinline controls></video>
+        <img v-show="!r" :src="'https://zengjunyong.github.io/rtc-h5/dist/h5/static/roomset_' + (i+2) + '.png'" alt="">
+      </div>
+    </div>
+    <div class="bottom">
+      <img :src="'https://zengjunyong.github.io/rtc-h5/dist/h5/static/camera' + (enableCamera ? '' : '-dis')  + '.png'"
+           @click="toggleCamera" alt="">
+      <img :src="'https://zengjunyong.github.io/rtc-h5/dist/h5/static/mic'+ (enableMic ? '' : '-dis') +'.png'"
+           @click="toggleMic" alt="">
+    </div>
   </div>
 </template>
 
 <script>
-import { formatTime } from '@/utils/index'
-import card from '@/components/card'
-
-export default {
-  components: {
-    card
-  },
-
-  data () {
-    return {
-      logs: []
+  export default {
+    name: "room",
+    data() {
+      return {
+        previewVideo: false,
+        remoteVideo: [false, false, false, false, false, false, false, false, false, false,
+          false, false, false, false, false, false, false, false, false],
+        lengthOfRemoteVideo: 0,
+        enableCamera: true,
+        enableMic: true
+      };
+    },
+    mounted() {
+    },
+    methods: {
+      toggleCamera() {
+        this.enableCamera = !this.enableCamera;
+        zg.enableCamera(this.enableCamera);
+      },
+      toggleMic() {
+        this.enableMic = !this.enableMic;
+        zg.enableMicrophone(this.enableMic);
+      }
     }
-  },
-
-  created () {
-    const logs = (wx.getStorageSync('logs') || [])
-    this.logs = logs.map(log => formatTime(new Date(log)))
-  }
-}
+  };
 </script>
 
-<style>
-.log-list {
-  display: flex;
-  flex-direction: column;
-  padding: 40rpx;
-}
 
-.log-item {
-  margin: 10rpx;
-}
+<style scoped lang="less">
+  .flex {
+    display: flex;
+    flex-direction: column;
+  }
+
+  @bottomHeight: 90px;
+
+  .scroll {
+    overflow: scroll;
+  }
+
+  .room {
+    flex-grow: 1;
+    padding: 12px 12px 0 12px;
+    display: flex;
+    flex-wrap: wrap;
+    height: calc(100% - @bottomHeight);
+
+    @margin: 2px;
+
+    .video {
+      width: calc(50% - @margin);
+      height: 50%;
+      background-color: #000;
+      margin-bottom: 6px;
+
+      &:nth-child(odd) {
+        margin-right: @margin;
+      }
+
+      &:nth-child(even) {
+        margin-left: @margin;
+      }
+
+      video {
+        width: 100%;
+        height: 100%;
+      }
+
+      image {
+        width: 30px;
+        height: 53px;
+      }
+    }
+
+    .opacity55 {
+      opacity: 0.55;
+    }
+
+  }
+
+  .bottom {
+    height: @bottomHeight;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0 25px;
+
+    image {
+      width: 50px;
+      height: 50px;
+    }
+  }
 </style>

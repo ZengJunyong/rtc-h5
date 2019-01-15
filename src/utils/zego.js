c10c082b7f6683f78856a9d4fef13d71d39ace7a
yong <zengjunyong@gmail.com>
2019-01-15 14:19:09 +0800

    refactor

--- src/utils/zego.js
+++ src/utils/zego.js
@@ -280,14 +280,6 @@ function enableScreen(enable) {
     loginRoom && zg.stopPublishingStream(_config.idName);
     loginRoom && zg.stopPreview(previewVideo);
 
-    var config = {
-      externalMediaStream: null,
-      width: 640,
-      height: 480,
-      frameRate: 15,
-      bitRate: 1000
-    };
-
     getBrowser() === "Firefox" && zg.startScreenShotFirFox("window", false, function(suc, mediastream) {
       console.log("startScreenShot:" + suc);
       screenCaptrue = suc;
@@ -295,7 +287,13 @@ function enableScreen(enable) {
       // 推送屏幕可有两种形式，一是作为流媒体直接推送 即下面这种形式
       //另一种是作为externalCapture，前提是需要先将流喂给video标签；，下面chrome推送方式就是这种形式；可任意选择其中之一
       if (loginRoom) {
-        doPreviewPublish(config);
+        doPreviewPublish({
+          externalMediaStream: null,
+          width: 640,
+          height: 480,
+          frameRate: 15,
+          bitRate: 1000
+        });
       }
     });
 
